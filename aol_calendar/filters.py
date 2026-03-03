1ac4c7666526d7a761962c6461acacaa294262c4
Alexey Kuzminich <akuzminich@gmail.com>
2026-03-03 16:00:55 +0800

    data: Обновил курсы за март-апрель-май
    и исправил ошибку парсинга имени "Свами Каушик (Каши)"

--- aol_calendar/filters.py
+++ aol_calendar/filters.py
@@ -1,6 +1,19 @@
+import re
+
+
+# "Свами Каушик (Каши)"
+fname_lname_nickname_re = re.compile(r'(\w+) (\w+) \((\w+)\)')
+
+
 def teacher_names(teachers):
     names = []
     for t in teachers:
-        last_name, first_name = t.split()
-        names.append(f'{last_name} {first_name[0]}.')
+        try:
+            last_name, first_name = t.split()
+            names.append(f'{last_name} {first_name[0]}.')
+        except Exception as e:
+            if (match := fname_lname_nickname_re.search(t)):
+                names.append(match.group(3))
+            else:
+                raise
     return ' + '.join(names)
